141e39674a7e36c3050c46f8310ae20d98c07665
Nass404 <tiasanas435@gmail.com>
2021-08-09 21:35:34 +0700

    Initial pertama

---
+++ Hello R.R
@@ -0,0 +1,15 @@
+"Hello Selamat Datang di Pemrograman R"
+
+nama = "Tias anas tastiana"
+nim = "A11.2020.12429"
+umur = 19
+ipk = 3.80
+jeniskelamin = "Laki-laki"
+prodi = "Teknik Informatika"
+
+print(nama)
+print(nim)
+print(umur)
+print(jeniskelamin)
+print(prodi)
+"Universitas Dian Nuswantoro"
